474851a80066b6c6df59f1ac17a96159b8250eef
George Harris <citizenharris@gmail.com>
2018-10-31 21:56:24 +0000

    Pass all Bob tests but one

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        statement.Trim();
+        switch(statement)
+        {
+            case var val when new Regex(@"^[\s]*$").IsMatch(val):
+                return "Fine. Be that way!";
+            case var val when new Regex(@"^[A-Z\s]+\?$").IsMatch(val):
+                return "Calm down, I know what I'm doing!";
+            case var val when new Regex(@"\?[\s]*$").IsMatch(val):
+                return "Sure.";
+            case var val when new Regex(@"^[A-Z]{1,}[^a-z]+$").IsMatch(val):
+                return "Whoa, chill out!";
+            default:
+                return "Whatever.";
+        } 
     }
 }
