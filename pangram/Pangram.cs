c4aeccb120e22348446e2e14f93842309d010981
George Harris <g.harris@reply.eu>
2018-10-31 17:20:29 +0000

    Refactor Pangram

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,34 +1,10 @@
-﻿﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Linq;
 
 public static class Pangram
 {
+    const string alphabet = "abcdefghijklmnopqrstuvwxyz";
     public static bool IsPangram(string input)
     {
-        if(input == "" || input.Length < 26)
-        {
-        	return false;
-        }
-
-        Dictionary<string, int> letterUsage = new Dictionary<string, int>();
-        
-        foreach(var letter in input.ToLower())
-        {
-        	string letterStr = letter.ToString();
-        	if(Regex.IsMatch(letterStr, @"[a-z]+"))
-        	{
-        		if(letterUsage.ContainsKey(letterStr))
-        		{
-        			letterUsage[letterStr]++;	
-        		}
-        		else
-        		{
-	        		letterUsage.Add(letterStr, 1);
-        		}
-        	}
-        }
-
-        return letterUsage.Count < 26 ? false : true;
+        return alphabet.All(input.ToLower().Contains);
     }
 }
