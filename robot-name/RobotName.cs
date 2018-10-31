3e1bc7b86ad62299233f01e65ae16f0c841ad2be
George Harris <citizenharris@gmail.com>
2018-10-31 20:47:51 +0000

    Solve robot-name

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,17 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
+    private string _name;
+    private List<string> _names = new List<string>();
+    public Robot() => Reset();
     public string Name
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+      get { return _name; }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        do
+        {
+            RoboName();
+        } while (_names.IndexOf(_name) != -1);
+        _names.Add(_name);
+    }
+
+    public void RoboName()
+    {
+        _name = string.Empty;
+        var rand = new Random();
+        for(int i = 0; i < 5; i++)
+        {
+            if(i< 2)
+            {
+                _name += (char)rand.Next('A', 'Z');
+            }
+            else
+            {
+                _name += (char)rand.Next('0', '9');
+            }
+        }
     }
 }
