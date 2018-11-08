f5efaa9ae238517f50d87547e82acde8e0889ef6
George Harris <citizenharris@gmail.com>
2018-11-08 21:08:28 +0000

    Solve perfect numbers

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+        
+        if (sum == number)
+        {
+            return Classification.Perfect;
+        }
+        else if (sum > number)
+        {
+            return Classification.Abundant;
+        }
+        else
+        {
+            return Classification.Deficient;
+        }
+    }
+}
