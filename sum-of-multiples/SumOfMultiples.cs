faea959645df12fb7f69c1f0c50ac2f6392b495e
George Harris <citizenharris@gmail.com>
2018-11-09 10:04:06 +0000

    Refactor sum of multiples

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        int sum = 0;
         HashSet<int> values = new HashSet<int>();
-        for (int i = 0; i < max; i++)
+        for (int i = 1; i < max; i++)
         {
-            if (i >= max) break;
             foreach (int num in multiples)
             {
-                if (i == 0 || num == 0) continue;
-                if (i % num == 0)
+                if (num != 0 && i % num == 0)
                 {
                     values.Add(i);
                 }
             }
         }
-        foreach (int num in values)
-        {
-            sum += num;
-        }
-        return sum;
+        return values.Sum();
     }
 }
