4553f4dfc72e10ddb0aae7f7e8c7a3e9083e6ca7
George Harris <citizenharris@gmail.com>
2018-11-14 19:39:28 +0000

    Triangle tests all pass

---
+++ triangle/Triangle.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Triangle
+{
+    public static bool IsScalene(double side1, double side2, double side3)
+    {
+        if (TriangleValidation(side1, side2, side3))
+        {
+            if (side1 != side2 && side1 != side3 && side2 != side3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsIsosceles(double side1, double side2, double side3) 
+    {
+        if (TriangleValidation(side1, side2, side3))
+        {
+            double[] sides = new double[3] {side1, side2, side3};
+            return sides.Distinct().Count() <= 2 ? true : false;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsEquilateral(double side1, double side2, double side3) 
+    {
+        if (TriangleValidation(side1, side2, side3))
+        {
+            if (side1 == side2 && side1 == side3)
+            {
+                return true;
+            } else 
+            {
+                return false;
+            }
+        } else 
+        {
+            return false;
+        }
+    }
+    public static bool TriangleValidation(double side1, double side2, double side3)
+    {
+        if (side1 < 0 || side2 < 0 || side3 < 0) throw new TriangleException();
+        if ((!(side1 + side2 >= side3) || !(side2 + side3 >= side1) || !(side1 + side3 >= side2) || (side1 == 0 || side2 == 0 || side3 == 0)))
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+    
+}
+
+
+public class TriangleException : Exception { }
