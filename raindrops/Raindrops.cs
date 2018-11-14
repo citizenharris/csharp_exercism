c858e612247f091aaea3141cd3fd18b5ba7604fe
George Harris <citizenharris@gmail.com>
2018-11-14 19:08:18 +0000

    Solve raindrops

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string output = string.Empty;
+        if (number % 3 == 0) output += "Pling";
+        if (number % 5 == 0) output += "Plang";
+        if (number % 7 == 0) output += "Plong";
+        if (output == "") return number.ToString();
+        return output;
+    }
+}
