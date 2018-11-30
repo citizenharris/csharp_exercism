b0e6ccbda040b717f29f727afe94ca70590b3af5
George Harris <citizenharris@gmail.com>
2018-11-30 11:36:12 +0000

    Solve error handling

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() => throw new Exception("An error occurred");
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (!int.TryParse(input, out int result))
+        {
+            return null;
+        }
+        else
+        {
+            return result;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) => int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using(disposableObject)
+        {
+            throw new Exception("Something went wrong.");
+        }
+    }
+}
