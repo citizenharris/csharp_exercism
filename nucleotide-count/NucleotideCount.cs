b9c189bf8a833df889f6fcf92f6dbe4f3f8474ea
George Harris <citizenharris@gmail.com>
2018-11-30 11:17:38 +0000

    Solve nucleotides

---
+++ nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        
+        if (new Regex(@"[^ACGT]").IsMatch(sequence)) throw new ArgumentException("Contains invalid nucleotides");
+        
+        var output = new Dictionary<char, int>
+        {
+            ['A'] = 0,
+            ['C'] = 0,
+            ['G'] = 0,
+            ['T'] = 0
+        };
+        foreach (var c in sequence.ToCharArray())
+        {
+            if (output.ContainsKey(c)) output[c] += 1;
+        }
+        return output;
+    }
+}
