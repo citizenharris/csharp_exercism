a33afbbe2a66a31cc3b397f11364bfa14920c6e9
George Harris <citizenharris@gmail.com>
2018-12-02 19:17:25 +0000

    Make small tweaks

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -6,56 +6,46 @@ public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        if (TriangleValidation(side1, side2, side3))
-        {
-            double[] sides = new double[3] {side1, side2, side3};
-            return sides.Distinct().Count() == 3 ? true : false;
-        }
-        else
+        if (!IsTriangleValid(side1, side2, side3))
         {
             return false;
         }
+        double[] sides = new double[3] {side1, side2, side3};
+        return sides.Distinct().Count() == 3 ? true : false;
     }
 
     public static bool IsIsosceles(double side1, double side2, double side3) 
     {
-        if (TriangleValidation(side1, side2, side3))
-        {
-            double[] sides = new double[3] {side1, side2, side3};
-            return sides.Distinct().Count() <= 2 ? true : false;
-        } 
-        else
+        if (!IsTriangleValid(side1, side2, side3))
         {
             return false;
-        }
+        } 
+        double[] sides = new double[3] {side1, side2, side3};
+        return sides.Distinct().Count() <= 2 ? true : false;
     }
 
     public static bool IsEquilateral(double side1, double side2, double side3) 
     {
-        if (TriangleValidation(side1, side2, side3))
-        {
-            double[] sides = new double[3] {side1, side2, side3};
-            return sides.All(x => x == side1) ? true : false;
-        } else 
+        if (!IsTriangleValid(side1, side2, side3))
         {
             return false;
-        }
+        } 
+        double[] sides = new double[3] {side1, side2, side3};
+        return sides.All(x => x == side1) ? true : false;
     }
 
-  public static bool TriangleValidation(double side1, double side2, double side3)
+  public static bool IsTriangleValid(double side1, double side2, double side3)
     {
         if (side1 < 0 || side2 < 0 || side3 < 0) throw new TriangleException();
         if ((!(side1 + side2 >= side3) || !(side2 + side3 >= side1) || !(side1 + side3 >= side2) 
             || (side1 == 0 || side2 == 0 || side3 == 0)))
         {
             return false;
-        } else
-        {
-            return true;
         }
+
+        return true;
     }
     
 }
-
 
 public class TriangleException : Exception { }
