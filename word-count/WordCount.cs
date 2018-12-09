2fffe9e6d7bef837df9edc8cdcac89bcd06383c3
George Harris <citizenharris@gmail.com>
2018-12-09 15:39:31 +0000

    Solve word count

--- word-count/WordCount.cs
+++ word-count/WordCount.cs
@@ -6,7 +6,7 @@ public static class WordCount
 {
     public static IDictionary<string, int> CountWords(string phrase)
     {
-        var words = Regex.Split(phrase.ToLower(), @"\W+");
+        string[] words = FormatPhrase(phrase);
 
         Dictionary<string, int> wordCount = new Dictionary<string, int>();
         foreach (var word in words)
@@ -21,7 +21,20 @@ public static class WordCount
                 wordCount[word]++;
             }
         }
-        
+
         return wordCount;
+    }
+
+    private static string[] FormatPhrase(string phrase)
+    {
+        var words = Regex.Split(phrase.ToLower(), @"[^(\w'\w)]");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+            if (Regex.IsMatch(word, @"'.+'")) words[i] = word.Replace("'", "");
+        }
+
+        return words;
     }
 }
