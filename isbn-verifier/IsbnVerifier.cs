a845f081a6493405d93aa317cf8a34c4d8e6b225
George Harris <citizenharris@gmail.com>
2018-12-10 21:10:56 +0000

    Abstract validation formula

---
+++ isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class IsbnVerifier
+{
+    public static bool IsValid(string number)
+    {
+        var isbn = number.Replace("-", "");
+        
+        int[] isbnNums = new int[isbn.Length];
+        for (int i = 0; i <= isbn.Length; i++)
+        {
+            if (!int.TryParse(isbn[i].ToString(), out int digit))
+            {
+                if (isbn[i] == 'X')
+                {
+                    isbnNums[i] = 10;
+                    continue;
+                }
+                return false;
+            }
+            isbnNums[i] = digit;
+        }
+        return ValidateIsbn(isbnNums);
+        
+    }
+
+    public static bool ValidateIsbn(int[] isbn)
+    {
+        int result = 0;
+        for (int i = 0; i < isbn.Length; i++)
+        {
+            result += isbn[i] * (isbn.Length - i);
+        }
+        return (result) % 11 == 0;
+    }
+
+}
