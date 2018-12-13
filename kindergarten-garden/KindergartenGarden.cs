13d22882cc2c7c890288a9bade6e5fd2c4886ea0
George Harris <citizenharris@gmail.com>
2018-12-13 10:54:28 +0000

    Version 2 with yield return

--- kindergarten-garden/KindergartenGarden.cs
+++ kindergarten-garden/KindergartenGarden.cs
@@ -12,29 +12,21 @@ public enum Plant
 
 public class KindergartenGarden
 {
-    private string _diagram;
-    private string _studentOrder = "ABCDEFGHIJKL";
-    public KindergartenGarden(string diagram) => _diagram = diagram;
+    private string[] diagram;
+    public KindergartenGarden(string diagram) => this.diagram = diagram.Split("\n");
 
     public IEnumerable<Plant> Plants(string student)
     {
-        var position = _studentOrder.IndexOf(student[0]) * 2;
-        
-        string[] output = _diagram.Split("\n");
-        output[0] = output[0].Substring(position, 2); 
-        output[1] = output[1].Substring(position, 2); 
-        
-        Plant[] plants = new Plant[4];
-        var index = 0;
-        foreach (var line in output)
+        var position = (student[0] - 'A') * 2;
+
+        for (int i = 0; i < diagram.Length; i++)
         {
-            foreach (var plant in line)
-            {
-                plants[index] = GetPlant(plant);
-                index++;
-            }
+            yield return GetPlant(diagram[0][position + i]);
         }
-        return plants;
+        for (int i = 0; i < diagram.Length; i++)
+        {
+            yield return GetPlant(diagram[1][position + i]);
+        }
     }
 
     public Plant GetPlant(char plant)
