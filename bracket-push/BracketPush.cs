47c121bc943712c38c69600c81b4941a4e19f780
George Harris <citizenharris@gmail.com>
2018-12-20 20:14:55 +0000

    Refactor to something less horrible

--- bracket-push/BracketPush.cs
+++ bracket-push/BracketPush.cs
@@ -1,91 +1,41 @@
 using System;
 using System.Collections.Generic;
-// using System.Linq;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public static class BracketPush
 {
     public static bool IsPaired(string input)
     {
-
-        input = input.Replace(@"[^\{\}\[\]\(\)]", "");
+        input = Regex.Replace(input, @"[^\{\}\[\]\(\)]", "");
         if (input == "") return true;
-
-        var bracketCount = new Dictionary<char, int>();
-        foreach (var bracket in input)
-        {
-            if (!bracketCount.ContainsKey(bracket)) bracketCount.Add(bracket, 0);
-            bracketCount[bracket]++;
-        }
-
-        foreach (var item in bracketCount)
-        {
-            switch (item.Key)
-            {
-                case '{':
-                    if (!bracketCount.ContainsKey('}')) return false;
-                    if (item.Value != bracketCount['}']) return false;
-                    break;
-                case '[':
-                    if (!bracketCount.ContainsKey(']')) return false;
-                    if (item.Value != bracketCount[']']) return false;
-                    break;
-                case '(':
-                    if (!bracketCount.ContainsKey(')')) return false;
-                    if (item.Value != bracketCount[')']) return false;
-                    break;
-                default:
-                    continue;
-            }
-        }
+        if (input.Length % 2 != 0) return false;
 
         var left = new List<char>() { '{', '[', '(' };
         var right = new List<char>() { '}', ']', ')' };
-        var indices = new List<int>();
-
+        
         for (int k = 0; k < 3; k++)
-        {
+        {    
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] == left[k])
                 {
+                    var indices = new List<int>();
                     var index = input.IndexOf(right[k], i);
                     if (index == -1) return false;
                     if (indices.Contains(index))
                     {
                         for (int j = i; j < input.Length - i; j++)
                         {
+                            if (indices.Contains(j)) continue;
                             if (input.IndexOf(right[k], j) == -1) return false;
-                            continue;
                         }
                     }
-                    indices.Add(input[i]);
+                    indices.Add(i);
                 }
             }
         }
 
-        return NextIndexOf(input) != -1;
-    }
-
-    public static int NextIndexOf(string input, int startIndex = 0)
-    {
-        for (int i = startIndex; i < input.Length; i++)
-        {
-            switch (input[i])
-            {
-                case '{':
-                    if (input.Contains('}')) return input.IndexOf('}', i);
-                    continue;
-                case '[':
-                    if (input.Contains(']')) return input.IndexOf(']', i);
-                    continue;
-                case '(':
-                    if (input.Contains(')')) return input.IndexOf(')', i);
-                    continue;
-                default:
-                    break;
-            }
-        }
-        return -1;
+        return true;
     }
 }
