b00446c7c947274f1fec805510a5114b4f1db890
George Harris <citizenharris@gmail.com>
2018-12-21 20:07:03 +0000

    Add two methods to handle hours/minutes overspill

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -4,24 +4,19 @@ public class Clock
 {
     public Clock(int hours, int minutes)
     {
-        Hours = hours;
-        Minutes = minutes;
+        Hours = FormatHours(hours);
+        Minutes = FormatMinutes(minutes).Item2;
+        if (minutes > 59) Hours += FormatMinutes(minutes).Item1;
     }
 
     public int Hours { get; set; }
-
     public int Minutes { get; set; }
 
     public Clock Add(int minutesToAdd)
     {
-        if (Minutes += minutesToAdd > 60) 
-        {
-            double asHours = minutesToAdd / 60;
-            int asHoursRounded = Math.Floor(asHours);
-            Hours += asHoursRounded;
-            Minutes += ((asHours) - asHoursRounded) * 60;
-        }
-        Minutes += minutesToAdd;
+        Hours += FormatMinutes(minutesToAdd).Item1;
+        Minutes += FormatMinutes(minutesToAdd).Item2;        
+        return this;
     }
 
     public Clock Subtract(int minutesToSubtract)
@@ -31,6 +26,22 @@ public class Clock
 
     public override string ToString()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return $"{Hours.ToString("D2")}:{Minutes.ToString("D2")}";
+    }
+
+    private static Tuple<int, int> FormatMinutes(int minutesToAdd)
+    {
+        if (minutesToAdd > 59)
+        {
+            double asHours = minutesToAdd / 60;
+            double asHoursRounded = Math.Floor(asHours);
+            return new Tuple<int, int>
+                (FormatHours((int)asHoursRounded), (int)((asHours) - asHoursRounded) * 60);
+        }
+        return new Tuple<int, int>(0, minutesToAdd);
+    }
+    private static int FormatHours(int hoursToAdd)
+    {
+        return hoursToAdd >= 24 ? hoursToAdd % 24 : hoursToAdd;
     }
 }
--- clock/ClockTest.cs
+++ clock/ClockTest.cs
@@ -11,70 +11,70 @@ public class ClockTest
         Assert.Equal("08:00", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Past_the_hour()
     {
         var sut = new Clock(11, 9);
         Assert.Equal("11:09", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Midnight_is_zero_hours()
     {
         var sut = new Clock(24, 0);
         Assert.Equal("00:00", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hour_rolls_over()
     {
         var sut = new Clock(25, 0);
         Assert.Equal("01:00", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hour_rolls_over_continuously()
     {
         var sut = new Clock(100, 0);
         Assert.Equal("04:00", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Sixty_minutes_is_next_hour()
     {
         var sut = new Clock(1, 60);
         Assert.Equal("02:00", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Minutes_roll_over()
     {
         var sut = new Clock(0, 160);
         Assert.Equal("02:40", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Minutes_roll_over_continuously()
     {
         var sut = new Clock(0, 1723);
         Assert.Equal("04:43", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hour_and_minutes_roll_over()
     {
         var sut = new Clock(25, 160);
         Assert.Equal("03:40", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hour_and_minutes_roll_over_continuously()
     {
         var sut = new Clock(201, 3001);
         Assert.Equal("11:01", sut.ToString());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hour_and_minutes_roll_over_to_exactly_midnight()
     {
         var sut = new Clock(72, 8640);
