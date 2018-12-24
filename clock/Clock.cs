053449a7582ee9ab920a7740aa840ea3ef8f2604
George Harris <citizenharris@gmail.com>
2018-12-24 15:04:21 +0000

    Develop FormatHours & FormatMinutes

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -4,44 +4,76 @@ public class Clock
 {
     public Clock(int hours, int minutes)
     {
-        Hours = FormatHours(hours);
-        Minutes = FormatMinutes(minutes).Item2;
-        if (minutes > 59) Hours += FormatMinutes(minutes).Item1;
+        Hours = minutes > 59 || minutes < 0?
+            FormatHours(hours + FormatMinutesAdd(minutes).Item1) : FormatHours(hours);
+        Minutes = FormatMinutesAdd(minutes).Item2;
     }
 
-    public int Hours { get; set; }
-    public int Minutes { get; set; }
+    public int Hours { get; private set; }
+    public int Minutes { get; private set; }
 
     public Clock Add(int minutesToAdd)
     {
-        Hours += FormatMinutes(minutesToAdd).Item1;
-        Minutes += FormatMinutes(minutesToAdd).Item2;        
+        Hours += FormatMinutesAdd(minutesToAdd).Item1;
+        Minutes += FormatMinutesAdd(minutesToAdd).Item2;
         return this;
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Hours -= FormatMinutesSubtract(minutesToSubtract).Item1;
+        Minutes -= FormatMinutesSubtract(minutesToSubtract).Item2;
+        return this;
     }
+
 
     public override string ToString()
     {
         return $"{Hours.ToString("D2")}:{Minutes.ToString("D2")}";
     }
 
-    private static Tuple<int, int> FormatMinutes(int minutesToAdd)
+    private static int FormatHours(int hoursToAdd)
+    {
+        if (hoursToAdd < 0) return 24 - ((hoursToAdd * -1) % 24);
+        return hoursToAdd >= 24 ? hoursToAdd % 24 : hoursToAdd;
+    }
+
+    private static void BreakDownMinutes(int minutesToAdd, out int hours, out int mins)
+    {
+        var asHours = ((double)minutesToAdd) / 60;
+        var asHoursRounded = Math.Floor(asHours);
+        mins = (int)Math.Ceiling(((asHours) - asHoursRounded) * 60);
+        hours = (int)asHoursRounded;
+    }
+
+    private static Tuple<int, int> FormatMinutesAdd(int minutesToAdd)
     {
         if (minutesToAdd > 59)
         {
-            double asHours = minutesToAdd / 60;
-            double asHoursRounded = Math.Floor(asHours);
+            BreakDownMinutes(minutesToAdd, out var hours, out var mins);
             return new Tuple<int, int>
-                (FormatHours((int)asHoursRounded), (int)((asHours) - asHoursRounded) * 60);
+                (FormatHours(hours), mins);
         }
-        return new Tuple<int, int>(0, minutesToAdd);
+        else if (minutesToAdd < 0)
+        {
+            minutesToAdd *= -1;
+            BreakDownMinutes(minutesToAdd, out var hours, out var mins);
+            return new Tuple<int, int>
+                    ((FormatHours(hours * -1)), (60 - mins));
+        } else
+        {
+            return new Tuple<int, int>(0, minutesToAdd);
+        }
     }
-    private static int FormatHours(int hoursToAdd)
+
+    private Tuple<int, int> FormatMinutesSubtract(int minutesToSubtract)
     {
-        return hoursToAdd >= 24 ? hoursToAdd % 24 : hoursToAdd;
+        if (minutesToSubtract > 59)
+        {
+            BreakDownMinutes(minutesToSubtract, out var hours, out var mins);
+            return new Tuple<int, int>
+                (FormatHours(hours), mins);
+        }
+        return new Tuple<int, int>(0, minutesToSubtract);
     }
 }
