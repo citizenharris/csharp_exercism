1e1dbb729ce945e69ef6a34e886f3f08ea844e42
George Harris <citizenharris@gmail.com>
2018-12-28 18:27:08 +0000

    Refactor

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -4,9 +4,11 @@ public class Clock : IEquatable<Clock>
 {
     public Clock(int hours, int minutes)
     {
-        Hours = minutes > 59 || minutes < 0?
-            FormatHours(hours + GetOverspillMinutes(minutes).Item1) : FormatHours(hours);
-        Minutes = minutes < 0 ? 60 - GetOverspillMinutes(minutes * - 1).Item2 : GetOverspillMinutes(minutes).Item2;
+        Hours = minutes > 59 ?
+            FormatHours(hours + GetOverspillMinutes(minutes).Item1) :
+                minutes < 0 ?
+                    FormatHours(GetOverspillMinutes(minutes).Item1) : FormatHours(hours);
+        Minutes = minutes < 0 ? 60 - GetOverspillMinutes(MakePositive(minutes)).Item2 : GetOverspillMinutes(minutes).Item2;
     }
 
     public int Hours { get; private set; }
@@ -15,9 +17,10 @@ public class Clock : IEquatable<Clock>
     public Clock Add(int minutesToAdd)
     {
         Hours += GetOverspillMinutes(minutesToAdd).Item1;
+        var overspillMinutes = GetOverspillMinutes(minutesToAdd).Item2;
         if (Minutes + minutesToAdd > 59)
         {
-            // Hours = FormatHours(Hours + 1);
+            Hours = Minutes + overspillMinutes > 59 ? FormatHours(Hours + 1) : Hours;
             Minutes = (Minutes + GetOverspillMinutes(minutesToAdd).Item2) % 60;
             return this;
         }
@@ -28,10 +31,11 @@ public class Clock : IEquatable<Clock>
     public Clock Subtract(int minutesToSubtract)
     {
         Hours -= GetOverspillMinutes(minutesToSubtract).Item1;
+        var overspillMinutes = GetOverspillMinutes(minutesToSubtract).Item2;
         if (Minutes - minutesToSubtract < 0)
         {
-            Hours = FormatHours(Hours - 1);
-            Minutes = 60 - GetOverspillMinutes(minutesToSubtract).Item2;
+            Hours = Minutes - overspillMinutes < 0 ? FormatHours(Hours - 1) : Hours;
+            Minutes = 60 - (MakePositive(Minutes - overspillMinutes % 60));
             return this;
         }
         Minutes -= GetOverspillMinutes(minutesToSubtract).Item2;
@@ -60,21 +64,23 @@ public class Clock : IEquatable<Clock>
         }
         else
         {
-            Clock p = (Clock)obj;
-            return (Hours == p.Hours) && (Minutes == p.Minutes);
+            Clock c = (Clock)obj;
+            return (Hours == c.Hours) && (Minutes == c.Minutes);
         }
     }
+
+    private int FormatHours(int hoursToAdd)
+    {
+        if (hoursToAdd < 0) return (MakePositive(hoursToAdd)) % 24 == 0 ? 0 : 24 - ((MakePositive(hoursToAdd)) % 24);
+        return hoursToAdd >= 24 ? hoursToAdd % 24 : hoursToAdd != 0 ? hoursToAdd : 0;
+    }
+
+    private int MakePositive(int number) => Math.Sign(number) < 0 ? number * -1 : number;
 
     private Tuple<int, int> GetOverspillMinutes(int minutes)
     {
         var overspill = minutes % 60;
         var loops = FormatHours((minutes - overspill) / 60);
         return new Tuple<int, int>(loops, overspill);
-    }
-
-    private static int FormatHours(int hoursToAdd)
-    {
-        if (hoursToAdd < 0) return 24 - ((hoursToAdd * -1) % 24);
-        return hoursToAdd >= 24 ? hoursToAdd % 24 : hoursToAdd != 0 ? hoursToAdd : 0;
     }
 }
