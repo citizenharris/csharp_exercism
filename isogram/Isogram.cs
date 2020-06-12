3a72ded8057ae650ac2418825aa6b10470e71265
George Harris <citizenharris@gmail.com>
2020-06-12 16:03:43 +0100

    Solve isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return true;
+            
+        word = word.ToLower().Replace(" ", "").Replace("-", "");
+        return word.Distinct().Count() == word.Count();
+    }
+}
