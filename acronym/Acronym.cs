7ff272c9d7d00c3320bfa6370c40663375e50fcb
George Harris <citizenharris@gmail.com>
2020-06-12 17:02:57 +0100

    Solve acronym

---
+++ acronym/Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var abbreviation = "";
+        Regex.Replace(phrase, @"[^a-zA-Z']+", " ")
+             .Split(" ").ToList().ForEach(x => abbreviation += (Char.ToUpper(x.First())));
+        return abbreviation;
+    }
+}
